e567c711313fab85d769944829d9c0246783416c
turbohoje <turbohoje@gmail.com>
2019-03-27 08:54:19 -0600

    variable to attempt to fix audio

--- powerbook_bt.sh
+++ powerbook_bt.sh
@@ -2,13 +2,16 @@
 # found here https://apple.stackexchange.com/questions/167245/yosemite-bluetooth-audio-is-choppy-skips/179209#179209
 # it would appear that maxing at 80 does not work well.  
 
-defaults write com.apple.BluetoothAudioAgent "Apple Bitpool Max (editable)" 40 
-defaults write com.apple.BluetoothAudioAgent "Apple Bitpool Min (editable)" 40 
-defaults write com.apple.BluetoothAudioAgent "Apple Initial Bitpool (editable)" 40 
-defaults write com.apple.BluetoothAudioAgent "Apple Initial Bitpool Min (editable)" 40 
-defaults write com.apple.BluetoothAudioAgent "Negotiated Bitpool" 40 
-defaults write com.apple.BluetoothAudioAgent "Negotiated Bitpool Max" 40 
-defaults write com.apple.BluetoothAudioAgent "Negotiated Bitpool Min" 40
+LOVAL=40
+HIVAL=80
+
+defaults write com.apple.BluetoothAudioAgent "Apple Bitpool Max (editable)" ${HIVAL}
+defaults write com.apple.BluetoothAudioAgent "Apple Bitpool Min (editable)" ${LOVAL}
+defaults write com.apple.BluetoothAudioAgent "Apple Initial Bitpool (editable)" ${HIVAL}
+defaults write com.apple.BluetoothAudioAgent "Apple Initial Bitpool Min (editable)" ${LOVAL}
+defaults write com.apple.BluetoothAudioAgent "Negotiated Bitpool" ${HIVAL}
+defaults write com.apple.BluetoothAudioAgent "Negotiated Bitpool Max" ${HIVAL}
+defaults write com.apple.BluetoothAudioAgent "Negotiated Bitpool Min" ${LOV${LOVAL}}
 
 #sanity check
 defaults read com.apple.BluetoothAudioAgent
